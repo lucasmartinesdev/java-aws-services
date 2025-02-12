bb0e02da22a052e5938a6ea726bac431d513dc2c
LucasMartines01 <lucas.martines@sptech.school>
2025-02-11 21:30:20 -0300

    finished integration with aws sm

---
+++ src/main/java/com/lucasmartines/awssecret/AwsSecretApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmartines.awssecret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSecretApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasmartines/awssecret/AwsSecretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmartines.awssecret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSecretApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
